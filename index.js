5b1001f9c41c3e06ef555c3052c74a2f1e16cf28
Brian Lin <lin.brian.l@gmail.com>
2017-10-31 16:49:33 -0500

    Delete commented portion.

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ $(document).ready(function() {
   $("li").on("click", function(event) {
     if (!game.over()) {
       var spaceIndex = parseInt($(this).attr("id"));
-
       var status = game.status(spaceIndex);
-
       if (status != "invalid") {
         renderMove(spaceIndex, game.currentPlayer());
         if (status === "win") {
@@ -24,26 +22,6 @@ $(document).ready(function() {
       } else {
         renderMessage(message, "invalidMove", null);
       }
-
-      // var result = game.move(spaceIndex);
-      // => {status: 'game won', winner: 'player 1'}
-
-      // if (game.validMove(spaceIndex)) {
-      //   game.placeMove(spaceIndex)
-      //   renderMove(spaceIndex, game.currentPlayer());
-      //   if (game.boardWin()) {
-      //     renderMessage(message, "win", game.currentPlayer())
-      //     renderWin(game);
-      //   } else if (!game.remainingMoves()) {
-      //     game.turnCount += 1;
-      //     renderMessage(message, "turn", game.currentPlayer());
-      //   } else {
-      //     renderMessage(message, "draw", null);
-      //     $(".replay").toggle();
-      //   }
-      // } else {
-      //   renderMessage(message, "invalidMove", null);
-      // }
     }
   });
 
